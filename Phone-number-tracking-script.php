00c4e19b872679ba3463651c2294d8c628dd7104
devnileshgeek <113500135+devnileshgeek@users.noreply.github.com>
2022-09-15 10:50:04 +0530

    Create Phone-number-tracking-script.php
    # Phone number tracking script.

---
+++ Phone-number-tracking-script.php
@@ -0,0 +1,8 @@
+<div class=”callus”>{TRACKING NUMBER}</div>
+<script>
+    window.__ctm_loaded = window.__ctm_loaded || [];
+    window.__ctm_loaded.push(function() { // this function is called once f.js loads
+      __ctm.main.start(document.querySelector(".callus"));
+    });
+</script>
+<script async src="//384375.tctm.co/t.js"></script>
